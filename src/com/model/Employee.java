6d7be0afd2bca8ee3a726bbc1cea6cbfa3816907
Sahasra Reddy <Sahasra Reddy@Sahasra>
2016-08-19 14:59:57 +0530

    package program

---
+++ src/com/model/Employee.java
@@ -0,0 +1,32 @@
+package com.model;
+
+public class Employee {
+
+private int empid;
+private String name;
+private String ssn;
+private double salary;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSsn() {
+	return ssn;
+}
+public void setSsn(String ssn) {
+	this.ssn = ssn;
+}
+public double getSalary() {
+	return salary;
+}
+public void setSalary(double salary) {
+	this.salary = salary;
+}}
